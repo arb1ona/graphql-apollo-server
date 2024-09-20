a37232dbba116ee0e53e839030a2affe62b80510
Arbiona <arbiona.shahu@gmail.com>
2024-09-20 22:48:54 +0200

    `resolver` functions

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
-import { typeDefs } from "./schema";
+import { typeDefs } from "./schema.js";
+// TypeDefs in GraphQL are a collection of definitions(types, queries, mutations)
+// that describe the shape and structure of the data your API can handle,
+//     enabling clients to interact with it in a predictable and well - defined way.
+import db from "./_db.js";
+
+/* Resolver is a collection of functions that generate response for a GraphQL query. */
+const resolvers = {
+  Query: {
+    games() {
+      return db.games;
+    },
+    authors() {
+      return db.authors;
+    },
+    reviews() {
+      return db.reviews;
+    },
+  },
+};
 
 const server = new ApolloServer({
   typeDefs,
-  // resolvers
+  resolvers,
 });
 
 const { url } = await startStandaloneServer(server, { listen: { port: 4000 } });
